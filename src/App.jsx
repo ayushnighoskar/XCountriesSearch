f0fedc3ccb61c32427b984dad5f32b5bce497d13
Ayush Nighoskar <ayunighoskar@gmail.com>
2025-01-06 23:46:59 +0530

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,12 @@
-/** @format */
-import axios from "axios";
+import XCountriesSearch from "./XCountriesSearch";
 import "./App.css";
-import { useEffect, useState } from "react";
-const App = () => {
-  const [search, setSearch] = useState("");
-  const [countries, setCountries] = useState([]);
 
-  useEffect(() => {
-    fetchCountries();
-  }, []);
-
-  const fetchCountries = async () => {
-    try {
-      const response = await axios.get("https://restcountries.com/v3.1/all");
-      setCountries(response.data);
-    } catch (error) {
-      console.error("something is wrong", error);
-    }
-  };
-
-  const filteredCountries = countries.filter((country) =>
-    country.name.common.toLowerCase().includes(search.toLowerCase())
-  );
-
+function App() {
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Search for countries..."
-        value={search}
-        className="searchInput"
-        onChange={(e) => setSearch(e.target.value)}
-      ></input>
-
-      <div className="countryGrid">
-        {filteredCountries.map((country) => {
-          return (
-            <div key={country.cca3} className="countryCard">
-              <img
-                src={country.flags.png}
-                alt={country.name.common}
-                className="flag"
-              />
-              <p className="countryName">{country.name.common}</p>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    <>
+      <XCountriesSearch />
+    </>
   );
-};
+}
 
 export default App;
